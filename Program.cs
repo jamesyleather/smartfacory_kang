41e0294ebd2f8afa3dd9c3e99e46ba1ef952c55c
jamesyleather <otlsaud@gmail.com>
2018-10-18 11:09:39 +0900

    the first version

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ricepot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.SetWindowSize(100, 40);
+            Random rand = new Random();
+            ConsoleColor[] Color = {ConsoleColor.Blue, ConsoleColor.Cyan,
+            ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green,
+            ConsoleColor.Magenta, ConsoleColor.Gray };
+
+            while (true)
+            {
+                Console.Clear();
+                for(int i = 0; i < 30; i++)
+                {
+                    System.Console.ForegroundColor = Color[rand.Next(7)];
+                    System.Console.SetCursorPosition(rand.Next(100), rand.Next(40));
+                    System.Console.Write("Hello World!");
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
