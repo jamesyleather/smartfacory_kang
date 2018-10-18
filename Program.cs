b265dabb57c3547697549f4f05791002590ced60
jamesyleather <otlsaud@gmail.com>
2018-10-18 11:41:20 +0900

    a compiled file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Ricepot
 {
